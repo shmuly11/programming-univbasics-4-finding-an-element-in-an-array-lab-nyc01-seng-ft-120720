94aa172ba2454a6e7f35a73908b973edc450dc03
shmuly levitin <shmuly11@gmail.com>
2020-11-17 19:18:57 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,10 @@
 def find_element_index(array, value_to_find)
   count = 0 
   while count < array.length 
+  binding.pry
   if array[count] == value_to_find
     return array[count]
-    binding.pry
+  
   end
   count += 1
 end
